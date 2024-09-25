f25a29658471cf5a72a6ca6cde07804d1c08b7d8
sanjay <sanjaykuttyags@gmail.com>
2024-09-25 12:36:34 +0530

    lasT

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
+// Connect to the database
 connectDB();
 
 const app = express();
 
+// Middleware for JSON parsing
 app.use(express.json());
+
+// Allow CORS for all origins
+app.use(cors());
 
 // Define routes
 app.use('/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
+// Root endpoint
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
